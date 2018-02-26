3078fb79d971e72268c672dd6e3de933d8f83696
Entec01 <alinamehdi15@gmail.com>
2018-02-26 11:47:47 -0500

    initial

---
+++ app/src/main/java/com/example/entec01/unit4lab3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.entec01.unit4lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Toast!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+}
